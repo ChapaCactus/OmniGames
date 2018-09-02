90db06168fbd75fef6f85f3e0e93c33706548d66
Chapa.C(Takuya) <chapatown@gmail.com>
2018-09-02 18:27:50 +0900

    戻るボタンのコールバック設定

--- Assets/Scripts/Global/GlobalUI.cs
+++ Assets/Scripts/Global/GlobalUI.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+using UnityEngine.UI;
+
 namespace CCG
 {
     public class GlobalUI : MonoBehaviour
     {
+        #region variables
+        [SerializeField]
+        private Button m_backToTitleButton = null;
+        #endregion
+
+        #region unity callbacks
+        private void Awake()
+        {
+            m_backToTitleButton.onClick.AddListener(OnClickBackToTitle);
+        }
+        #endregion
+
         #region public methods
         public static GlobalUI Create()
         {
@@ -14,9 +28,7 @@ namespace CCG
             var entity = Instantiate(prefab, null);
             return entity;
         }
-        #endregion
 
-        #region public methods
         public void Init()
         {
             SetActive(false);
@@ -25,6 +37,12 @@ namespace CCG
         public void SetActive(bool isActive)
         {
             gameObject.SetActive(isActive);
+        }
+        #endregion
+
+        #region private methods
+        private void OnClickBackToTitle()
+        {
         }
         #endregion
     }
