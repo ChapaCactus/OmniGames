28b20f0aba32de8dd35cd61ba05c3c9988a09b58
Chapa.C(Takuya) <chapatown@gmail.com>
2018-09-02 18:10:42 +0900

    .Net4.6に引き上げ、Globalクラス追加

---
+++ Assets/Scripts/Global/Global.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public static class Global
+    {
+        #region properties
+        public static bool IsInitialized { get; private set; } = false;
+        #endregion
+
+        #region public methods
+        public static void Init()
+        {
+            IsInitialized = true;
+        }
+        #endregion
+    }
+}
