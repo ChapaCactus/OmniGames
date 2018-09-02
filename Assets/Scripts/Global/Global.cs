b1b12462be23b52e6adac7b03607cdab03e85b35
Chapa.C(Takuya) <chapatown@gmail.com>
2018-09-02 18:17:03 +0900

    GlobalUI初期化

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -8,12 +8,16 @@ namespace CCG
     public static class Global
     {
         #region properties
+        public static GlobalUI GlobalUI { get; private set; }
+        // ゲーム初期化済みか
         public static bool IsInitialized { get; private set; } = false;
         #endregion
 
         #region public methods
         public static void Init()
         {
+            GlobalUI = GlobalUI.Create();
+
             IsInitialized = true;
         }
         #endregion
---
+++ Assets/Scripts/Global/GlobalUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public class GlobalUI : MonoBehaviour
+    {
+        #region public methods
+        public static GlobalUI Create()
+        {
+            var prefab = Resources.Load("") as GlobalUI;
+            var entity = Instantiate(prefab, null);
+            return entity;
+        }
+        #endregion
+    }
+}
