d525a3a8d9bbe545682b5a8d5fe82298585d7b35
Chapa.C(Takuya) <chapatown@gmail.com>
2018-09-05 17:16:50 +0900

    雪壁の追加

---
+++ Assets/Scripts/SnowballFight/SnowWall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public class SnowWall : MonoBehaviour
+    {
+        #region public methods
+        public void Break()
+        {
+        }
+        #endregion
+    }
+}
