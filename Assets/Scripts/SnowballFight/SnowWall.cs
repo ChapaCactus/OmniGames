b3cec87af2be8f9e0245ca7f07538b914449e2a1
Chapa.C(Takuya) <chapatown@gmail.com>
2018-09-05 17:27:00 +0900

    雪壁調整

--- Assets/Scripts/SnowballFight/SnowWall.cs
+++ Assets/Scripts/SnowballFight/SnowWall.cs
@@ -7,9 +7,28 @@ namespace CCG
 {
     public class SnowWall : MonoBehaviour
     {
+        #region properties
+        public Enum.Side Side { get; private set; }
+        #endregion
+        
         #region public methods
+        public void Setup(Enum.Side side)
+        {
+            Side = side;
+
+            // サイドによって向きを変えておく
+            var zRotate = (side == Enum.Side.Player1) ? 0 : 180;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, zRotate));
+        }
+
+        public void SetActive(bool isActive)
+        {
+            gameObject.SetActive(isActive);
+        }
+
         public void Break()
         {
+            SetActive(false);
         }
         #endregion
     }
